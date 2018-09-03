5ce13eb8f0c746600e014a9f901e91ac666dedfd
psat93 <p.sateler93@gmail.com>
2018-09-03 10:39:39 -0300

    dsad

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,10 +4,17 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 
 class QuizQuestion extends Component{
+  constructor(props){
+    super(props)
+    this.state= {incorrectAnswer: false}
+  }
     handleClick(buttonText){      
       if(buttonText===this.props.quiz_question.answer){
         this.props.showNextQuestionHandler()
       }    
+      this.setState ((state)=>{
+        return { incorrectAnswer: (state.incorrectAnswer ? false : true)}        
+    }) 
     }
     render(){
         return <main>
@@ -23,6 +30,7 @@ class QuizQuestion extends Component{
             
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error"> Sorry, thats not right </p> : null}
       </main>
     }
 }
