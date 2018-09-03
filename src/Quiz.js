948e9208ec7ac5b92cf9dbf2b8d159525f873d87
psat93 <p.sateler93@gmail.com>
2018-09-03 10:25:49 -0300

    avva

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,20 +3,31 @@ import QuizQuestion from './QuizQuestion';
 import QuizEnd from './QuizEnd';
 let quizData = require('./quiz_data.json');
 
+
+
 class Quiz extends Component {
     constructor(props){
-        super(props),
+        super(props)
         this.state = {quiz_position: 1};
+    }
+    showNextQuestion(){
+        console.log(this.state.quiz_position);
+        this.setState ((state)=>{
+            return { quiz_position: state.quiz_position + 1}
+        })     
+        console.log(this.state.quiz_position);   
+        
     }
     
     render(){
-        const isQuizEnd = this.state.quiz_position-1 == quizData.quiz_questions.length ? true : false;
-        return <div>
+        const isQuizEnd = ((this.state.quiz_position-1) === quizData.quiz_questions.length);
+        return (<div>
             <div className="QuizQuestion">
             {isQuizEnd ? <QuizEnd /> :
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} /> }
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler=
+            {this.showNextQuestion.bind(this)} /> }
             </div>
-        </div>
+        </div>)
     }
 }
 
